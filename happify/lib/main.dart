714eea95520e49fdb292d5e2bff36d25ca40fb67
KunalManiyar <kunal.maniyar@somaiya.edu>
2021-09-19 17:59:36 +0530

    signin and profile

--- happify/lib/main.dart
+++ happify/lib/main.dart
@@ -1,113 +1,269 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Profile());
 }
 
-class MyApp extends StatelessWidget {
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
   // This widget is the root of your application.
+  bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          currentFocus.focusedChild!.unfocus();
+        }
+      },
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            leading: GestureDetector(
+              child: Icon(Icons.menu),
+            ),
+            title: Text(
+              "SIGN IN",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.orange[600],
+          ),
+          body: Container(
+            padding: EdgeInsets.symmetric(horizontal: 36.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Email id',
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: SizedBox(
+                          width: 250,
+                          child: TextField(
+                            onChanged: (value) {
+                              //Do something with the user input.
+                            },
+                            decoration: InputDecoration(
+                              hintText: 'john_doe@gmail.com',
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0,
+                                horizontal: 20.0,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.orange.shade600, width: 2.0),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(16.0)),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Password',
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: SizedBox(
+                          width: 250,
+                          child: TextField(
+                            obscureText: _isObscure,
+                            onChanged: (value) {
+                              //Do something with the user input.
+                            },
+                            decoration: InputDecoration(
+                              hintText: 'xxxxxx',
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _isObscure
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _isObscure = !_isObscure;
+                                  });
+                                },
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0,
+                                horizontal: 20.0,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.orange.shade600, width: 2.0),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(16.0)),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 60.0),
+                SizedBox(
+                    height: 45.0,
+                    width: 300.0,
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.orange[600],
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                        ),
+                      ),
+                      onPressed: () {},
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0),
+                      ),
+                    )),
+                SizedBox(height: 50.0),
+                Text(
+                  "Or Sign Up Using",
+                  style: TextStyle(fontSize: 22.0, color: Colors.black),
+                ),
+                SizedBox(height: 10.0),
+                SizedBox(
+                  width: 150.0,
+                  child: TextButton(
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: true,
+          title: Text(
+            "Profile",
+            style: TextStyle(
+              fontSize: 30,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.orange[600],
+        ),
+        body: Container(
+          padding: EdgeInsets.all(36.0),
+          child: Column(
+            children: [
+              Stack(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 100.0),
+                    height: 200,
+                    width: 200,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.orange[600],
+                      radius: 100,
+                      child: Icon(
+                        Icons.person,
+                        size: 180.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 70.0,
+                    width: 70.0,
+                    margin: EdgeInsets.only(top: 220.0, left: 140.0),
+                    child: FloatingActionButton(
+                      onPressed: () {
+                        // Add your onPressed code here!
+                      },
+                      child: const Icon(
+                        Icons.add,
+                        size: 60,
+                      ),
+                      backgroundColor: Colors.orange[600],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    margin: EdgeInsets.only(top: 185.0),
+                    child: SizedBox(
+                        height: 45.0,
+                        width: 300.0,
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          color: Colors.orange[600],
+                          child: Text(
+                            "Done",
+                            style: TextStyle(
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          onPressed: () {},
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0),
+                          ),
+                        )),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
