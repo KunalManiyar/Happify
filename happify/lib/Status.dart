a70c45b3bf030301165251644afe77d2c201bcfc
KunalManiyar <kunal.maniyar@somaiya.edu>
2021-10-11 15:55:28 +0530

    Relations and Status added

---
+++ happify/lib/Status.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Status extends StatefulWidget {
+  const Status({ Key? key }) : super(key: key);
+
+  @override
+  _StatusState createState() => _StatusState();
+}
+
+class _StatusState extends State<Status> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
